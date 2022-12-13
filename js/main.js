47c5687e0eea983036cdc66b627821dd9b43c33a
noyaaljohny <113782227+noyaaljohny@users.noreply.github.com>
2022-12-13 06:26:49 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+console.log("JavaScript Portion of the Test");
+
+// Step 1: Write a multi-line comment detailing your name and student number
+
+
+// Step 2: Create a block scoped, immutable variable that stores an empty array
+
+
+// Step 3: Add the 3 people's names to the array
+
+
+// Step 4: Remove the second array item from the array
+
+
+// Step 5: Using array map, iterate over the array items and make them all uppercase. Store the result in a new, block scoped, mutable variable
+
+
+// Step 6: Create an event listener that fires a callback function when the aside h2 is clicked. It should alert "Hello from Boo the World's Cutest Dog!";
